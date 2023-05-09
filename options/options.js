0fb618c1e2f003242f9d56f6f5bb549d852f3fad
Crowdbotics <team@crowdbotics.com>
2023-05-09 02:53:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-field-40567",
+  url: "https://twilight-field-40567.botics.co",
+  api: "https://twilight-field-40567.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
